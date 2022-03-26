0f142f3f7adb559766f796e15a43a26d846fa121
IlliyaF <yiliy749@gmail.com>
2022-03-26 13:50:20 +0200

    Commit to b2

---
+++ src/IndependObserv.java
@@ -0,0 +1,7 @@
+public class IndependObserv {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("b1");
+        System.out.println("b2");
+    }
+}
